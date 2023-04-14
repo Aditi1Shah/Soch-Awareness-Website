fde0db73f7394df72354661ea46f9e80bf43313b
Aditi Shah <aish6816@gmail.com>
2023-04-15 00:41:51 +0530

    Context API

---
+++ awareness/src/Context/Reducer.js
@@ -0,0 +1,26 @@
+const Reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN_START":
+      return {
+        user: null,
+        isFetching: true, //we have to begin the process of login
+        error: false, //As we are still fetching there can be no error
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        user: action.payload,
+        isFetching: false, //User has successfully logged in no more need to fetch info
+        error: false, //As we are still fetching there can be no error
+      };
+    case "LOGIN_FAILURE":
+      return {
+        user: null,
+        isFetching: false, //we have to begin the process of login
+        error: true,
+      };
+    default:
+      return state;
+  }
+};
+export default Reducer;
+//"proxy": "http://127.0.0.1:5000/",
--- awareness/src/Login/login.jsx
+++ awareness/src/Login/login.jsx
@@ -1,29 +1,58 @@
+import { useRef, useContext } from "react";
 import "./login.css";
+import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import { Context } from "../Context/Context";
 export default function Login() {
+  const userRef = useRef();
+  const PasswordRef = useRef();
+  const { user, dispatch, isFetching } = useContext(Context);
+
   const navigate = useNavigate();
   const handleRegister = (e) => {
     let path = "/register";
     navigate(path);
     e.preventDefault();
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" }); //its going to go to action.js and turn isfetching true
+    //then we will call api
+    try {
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: PasswordRef.current.value,
+      });
+      //if we have response means login successful
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    } catch (err) {
+      //if error means login failure
+      dispatch({ type: "LOGIN_FAILURE" });
+    }
+  };
+  console.log(user);
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
-        <label>Email</label>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
         <input
           type="text"
           className="loginInput"
-          placeholder="Enter your email..."
+          placeholder="Enter your username..."
+          ref={userRef}
         />
         <label>Password</label>
         <input
           type="password"
           className="loginInput"
           placeholder="Enter your password..."
+          ref={PasswordRef}
         />
-        <button className="loginButton">Login</button>
+        <button className="loginButton" type="submit">
+          Login
+        </button>
       </form>
       <button className="loginRegisterButton" onClick={handleRegister}>
         Register
