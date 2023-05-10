bd726fb0de01b3536b15fd81eae8526ada76d1a6
Aditi Shah <aish6816@gmail.com>
2023-05-10 20:34:25 +0530

    Top Bar Settings Updated

--- awareness/src/topiconbar/Topiconbar.jsx
+++ awareness/src/topiconbar/Topiconbar.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import "./topiconbar.css"
+import React from "react";
+import "./topiconbar.css";
 export default function Topiconbar() {
   return (
     <div className="topiconbar">
-      <div className='topiconbarleft'>
-      <i className="topicon fa-brands fa-square-facebook"></i>
-      <i className="topicon fa-brands fa-instagram"></i>
-      <i className="topicon fa-brands fa-twitter"></i>
-      <i className="topicon fa-brands fa-linkedin"></i>
-      <i className="topicon fa-brands fa-youtube"></i>
+      <div className="topiconbarleft">
+        <i className="topicon fa-brands fa-square-facebook"></i>
+        <i className="topicon fa-brands fa-instagram"></i>
+        <i className="topicon fa-brands fa-twitter"></i>
+        <i className="topicon fa-brands fa-linkedin"></i>
+        <i className="topicon fa-brands fa-youtube"></i>
       </div>
-      <div className='topiconbarright'>
-      <i className="tophomeicon fa-solid fa-house-chimney"></i>
-      <p className='address'>60, 3rd Avenue, Angel's Colony, Bangalore, Karnataka, 560034|</p>
-      <i className=" tophomeicon fa-sharp fa-solid fa-phone"></i>
-      <p className='phone'>973823929</p>
+      <div className="topiconbarright">
+        <i className="tophomeicon fa-solid fa-house-chimney"></i>
+        <p className="address">
+          60, 3rd Avenue, Angel's Colony, Bangalore, Karnataka, 560034|
+        </p>
+        <i className=" tophomeicon fa-sharp fa-solid fa-phone"></i>
+        <p className="phone">0000000000</p>
       </div>
     </div>
-  )
+  );
 }
